c5e93572502c7ecb9647cbbea5870e2074c175c8
tkhuynh <thequest2311@yahoo.com>
2016-12-06 19:37:12 -0800

    notify incoming msg id conversation minimized, or open window if window closed

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -23,6 +23,7 @@ class MessageBroadcastJob < ApplicationJob
   def broadcast_to_recipient(user, message)
   	ActionCable.server.broadcast(
   		"conversations-#{user.id}",
+      window: render_window(message.conversation, user),
   		message: render_message(message, user),
   		conversation_id: message.conversation_id
   	)
@@ -38,5 +39,15 @@ class MessageBroadcastJob < ApplicationJob
   			user: user
   		}
   	)
+  end
+
+  def render_window(conversation, user)
+    ApplicationController.render(
+      partial: 'conversations/conversation',
+      locals: {
+        conversation: conversation,
+        user: user
+      }
+    )
   end
 end
